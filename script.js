1c52e8d217a2e86ea0a96c784d30a64229a65ab2
danilexy <dannyboysmiles@gmail.com>
2023-06-06 19:12:53 +0100

    New homepage guys

--- script.js
+++ script.js
@@ -16,3 +16,27 @@ const nav = document.querySelector('.row-2');
 open.onclick = function() {
     nav.classList.toggle("active");
 }
+
+var x = document.getElementById("login")
+var y = document.getElementById("register")
+var z = document.getElementById("btn")
+const button = document.querySelector('.get-started');
+const formBox = document.querySelector('.form-box');
+const shapeImg = document.querySelector('.shape-img');
+
+button.addEventListener('click', () => {
+    shapeImg.classList.add('none');
+    formBox.classList.add('show');
+});
+
+function register(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "110px";
+    
+}
+function login(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "0"; 
+}
