98c1b1f12673d11631a9a24ebf435b9eb8dd8b62
Emmanuel <emmanuelmark501@gmail.com>
2023-06-06 11:30:09 -0700

    yusuv

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ const nav = document.querySelector('.row-2');
 open.onclick = function() {
     nav.classList.toggle("active");
 }
+
+function scrollImage(direction) {
+  const imageScroller = document.querySelector('.blogg-img');
+  const scrollAmount = 100; // Adjust the scroll distance as desired
+
+  if (direction === 'left') {
+    imageScroller.scrollLeft -= scrollAmount;
+  } else if (direction === 'right') {
+    imageScroller.scrollLeft += scrollAmount;
+  }
+}
